0bd57fa0bb606d54a483118c895a3dd1ca26e8bc
shashank <shashanksmf@outlook.com>
2018-03-30 00:23:58 +0530

    env file committed

--- src/env.js
+++ src/env.js
@@ -1,5 +1,6 @@
 export default {
-    Api_port: 3000, // Your Free Port For this server
-    App_key: 'gglgm"7_"Tx?MT-eyY_.[EoElhX6Fl', // unique string for jwt
-    DB_URL: 'mongodb://localhost:27017/test' // Your mongodb string
+  Api_port: 3000, // Your Free Port For this server
+  App_key: 'gglgm"7_"Tx?MT-eyY_.[EoElhX6Fl', // unique string for jwt
+  // DB_URL: 'mongodb://localhost:27017/test' // Your mongodb string
+  DB_URL: 'mongodb://admin:admin@heroku_1qmzqz96.mlab.com:11529/heroku_1qmzqz96'
 }
