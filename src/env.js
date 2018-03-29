9ad0d12ed339bf0655b6cbc9ac297a7434e6cd45
shashank <shashanksmf@outlook.com>
2018-03-30 00:33:44 +0530

    mongo uri modified

--- src/env.js
+++ src/env.js
@@ -2,5 +2,5 @@ export default {
   Api_port: process.env.PORT || 3000, // Your Free Port For this server
   App_key: 'gglgm"7_"Tx?MT-eyY_.[EoElhX6Fl', // unique string for jwt
   // DB_URL: 'mongodb://localhost:27017/test' // Your mongodb string
-  DB_URL: 'mongodb://admin:admin@heroku_1qmzqz96.mlab.com:11529/heroku_1qmzqz96'
+  DB_URL: 'mongodb://admin:admin@ds111529.mlab.com:11529/heroku_1qmzqz96'
 }
